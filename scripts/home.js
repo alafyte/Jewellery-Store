482b46a7b94a4f59f2dbcbdba0fc076fa73d9c7d
alafyte <xlebxlebxleb0508@gmail.com>
2022-03-06 15:04:02 +0300

    Home page content added

--- scripts/home.js
+++ scripts/home.js
@@ -3,21 +3,19 @@ var images = [
     'files/sale-2022-01-1440x520.jpg', 
     'files/драгоценности-01-1440x520.jpg',
     'files/обручалка_new-03-1440x520.jpg',
-    'files/часы-09-1440x520.jpg'
 ];
 var imagesSources = [
     'Page-1.html',
     'Page-2.html'
 ];
 var num = 0;
-setInterval("next();", 7000);
 
 function prev() {
     var slider = document.getElementById('slider');
     var sliderSource = document.getElementById('slider-source');
     num--;
     if(num < 0) {
-        num = images.length-1;
+        num = images.length - 1;
     }
     slider.src = images[num];
     sliderSource.href = imagesSources[num];
